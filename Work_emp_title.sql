981f909b1b3e7e77fc5b3a090d437ef43eb54621
Lalito1997 <125767088+Lalito1997@users.noreply.github.com>
2023-05-16 14:41:03 -0500

    Update Work_emp_title.sql
    deleted drop table

--- Work_emp_title.sql
+++ Work_emp_title.sql
@@ -70,5 +70,3 @@ REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
-
-drop table dept_manager;
